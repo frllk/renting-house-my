bcb07b3f46475e1870b10dedd9bd6f7975f238a8
frllk <1402805300@qq.com>
2020-07-21 16:37:38 +0800

    按需引入UI组件

--- src/views/News/index.jsx
+++ src/views/News/index.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Styles from './index.module.css'
+import { Button } from 'antd-mobile';
 
 export default class News extends Component {
   render () {
     return (
       <div className={Styles.test}>
-        News
+        <Button type="warning">warning</Button>
+        <Button type="ghost" className="am-button-borderfix">ghost disabled</Button>
       </div>
     )
   }
